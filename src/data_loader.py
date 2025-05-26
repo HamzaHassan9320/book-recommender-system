d6f3706727413772636dbc9d799c3eccddbe3f35
Hamza Hassan <hamza_hassan45@hotmail.com>
2025-05-26 23:02:46 +0100

    Add Gradio Dashboard

--- src/data_loader.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def data_loader(file_path: str) -> pd.DataFrame:
-    df = pd.read_csv(file_path)
-    return df
-
-books_df = data_loader(r"data/books.csv")
-print(books_df.shape)
-print(books_df.head())
